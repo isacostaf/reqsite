333957f560d9214bd0987a3b34eb2154c0981eb4
isacostaf <iisabellecostaf@gmail.com>
2025-05-06 14:32:38 -0300

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
+const repo = 'reqsite';
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/your-repository-name/' : '',
-  basePath: isProd ? '/your-repository-name' : '',
+  assetPrefix: isProd ? '/reqsite/' : '',
+  basePath: isProd ? '/reqsite' : '',
   output: 'export'
 };
 
